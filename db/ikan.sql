28daa8e0d66d56d08bcc036326c3dd6390710e98
Muhammad Haitsam <54610524+mhaitsam18@users.noreply.github.com>
2023-07-18 22:49:17 +0700

    Update ikan.sql

--- db/ikan.sql
+++ db/ikan.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 18, 2023 at 04:09 PM
+-- Generation Time: Jul 18, 2023 at 05:48 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.1.17
 
